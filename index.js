fb1664025b0acdcfaeca5ef62a987c12f4da1be6
Michal Jezierski <ambinanitelo@gmail.com>
2017-05-02 00:56:34 +0200

    eslint spacing rules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: ['react'],
     rules: {
+        'arrow-spacing': 'warn',
         'comma-dangle': ['warn', 'never'],
         'comma-spacing': ['warn', {
             'before': false,
@@ -8,6 +9,7 @@ module.exports = {
         }],
         'eqeqeq': 'warn',
         'indent': ['warn', 4, { 'SwitchCase': 1 }],
+        'keyword-spacing': 'warn',
         'linebreak-style': ['warn', 'unix'],
         'max-len': ['warn', 120, 4, { 'ignoreUrls': true }],
         'no-undef': ['warn'],
@@ -18,6 +20,7 @@ module.exports = {
         'react/jsx-uses-react': 'error',
         'react/jsx-uses-vars': 'error',
         'semi': ['warn', 'always'],
+        'space-before-blocks': 'warn',
         'space-infix-ops': ['warn']
     },
     env: {
