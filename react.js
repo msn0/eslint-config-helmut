e82a72bba1b35e1166ea678b85b482c92632b681
Michał Jezierski <ambinanitelo@gmail.com>
2017-10-06 14:15:46 +0200

    warning instead of error

--- react.js
+++ react.js
@@ -1,8 +1,8 @@
 module.exports = {
     plugins: ['react'],
     rules: {
-        'react/jsx-curly-spacing': ['error', { when: 'always', spacing: { objectLiterals: 'never' } }],
-        'react/jsx-uses-react': 'error',
-        'react/jsx-uses-vars': 'error'
+        'react/jsx-curly-spacing': ['warn', { when: 'always', spacing: { objectLiterals: 'never' } }],
+        'react/jsx-uses-react': 'warn',
+        'react/jsx-uses-vars': 'warn'
     }
 };
